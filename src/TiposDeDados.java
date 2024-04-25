20723637c352af64d2a6561f7585e74d796768bd
Romulo Teixeira <teixeira.romulo@hotmail.com>
2024-04-24 21:52:23 -0300

    (build) - criando repositorio das aulas

---
+++ src/TiposDeDados.java
@@ -0,0 +1,18 @@
+public class TiposDeDados {
+    public static void main(String[] args) {
+
+        //tipo de dado inteiro
+        int inteiro = 10;
+
+        //tipo de dado booleano
+        boolean verdadeiroOuFalso = true;
+
+        //tipo de dado String normalmente utilizado para texto
+        String nome = "Romulo";
+
+        //tipo de dado double, numeros não exatos
+        double salario = 1500.00;
+
+
+    }
+}
